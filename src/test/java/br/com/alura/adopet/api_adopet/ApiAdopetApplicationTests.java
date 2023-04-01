bdb712a800095e34429e462203c2d95a1215963c
Alisson Jaques <alissonjaquesrmq@gmail.com>
2023-04-01 13:16:20 -0300

    Criação do projeto com configurações básicas iniciais

---
+++ src/test/java/br/com/alura/adopet/api_adopet/ApiAdopetApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.alura.adopet.api_adopet;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiAdopetApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
